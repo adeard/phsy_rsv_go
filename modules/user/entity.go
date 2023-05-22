d530c61d1fcab161b332fd4e9ee9d8bc7554bc1c
adeard <adeardian1994@gmail.com>
2023-05-22 16:57:06 +0700

    add user type value

--- modules/user/entity.go
+++ modules/user/entity.go
@@ -1,20 +1,23 @@
 package user
 
 import (
+	usertype "phsy_rsv_go/modules/user_type"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	Username    string `gorm:"size:255;not null;unique" json:"username"`
-	Password    string `gorm:"size:255;not null;" json:"password"`
-	UserTypeId  int    `gorm:"default:1" json:"user_type_id"`
-	UserLevelId int    `gorm:"default:1" json:"user_level_id"`
-	Name        string `gorm:"size:255;default:null" json:"name"`
-	Email       string `gorm:"size:255;default:null" json:"email"`
-	Address     string `gorm:"default:null" json:"address"`
-	ImgProfile  string `gorm:"size:255;" json:"img_profile"`
-	Gender      string `gorm:"size:255;default:male" json:"gender"`
-	BirthDate   string `gorm:"type:date;default:null" json:"birth_date"`
-	IsActive    bool   `gorm:"default:true;" json:"is_active"`
+	Username    string            `gorm:"size:255;not null;unique" json:"username"`
+	Password    string            `gorm:"size:255;not null;" json:"password"`
+	UserTypeId  int               `gorm:"default:1" json:"user_type_id"`
+	UserLevelId int               `gorm:"default:1" json:"user_level_id"`
+	Name        string            `gorm:"size:255;default:null" json:"name"`
+	Email       string            `gorm:"size:255;default:null" json:"email"`
+	Address     string            `gorm:"default:null" json:"address"`
+	ImgProfile  string            `gorm:"size:255;" json:"img_profile"`
+	Gender      string            `gorm:"size:255;default:male" json:"gender"`
+	BirthDate   string            `gorm:"type:date;default:null" json:"birth_date"`
+	IsActive    bool              `gorm:"default:true;" json:"is_active"`
+	UserType    usertype.UserType `json:"user_type"`
 }
--- modules/user/repository.go
+++ modules/user/repository.go
@@ -28,7 +28,7 @@ func (r *repository) FindAll() ([]User, error) {
 
 func (r *repository) FindByID(ID int) (User, error) {
 	var user User
-	err := r.db.Where("id = ?", ID).First(&user).Error
+	err := r.db.Preload("UserType").Where("id = ?", ID).First(&user).Error
 
 	return user, err
 }
